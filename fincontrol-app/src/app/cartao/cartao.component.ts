f1c4e489e047f312dbbb87074e64fe0d12b3e028
Jader de Carvalho <decarvalhojader@gmail.com>
2020-02-26 21:20:27 -0300

    Layout do cadastro e alteração de cartões

--- fincontrol-app/src/app/cartao/cartao.component.ts
+++ fincontrol-app/src/app/cartao/cartao.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartaoComponent implements OnInit {
 
+  isCadastro: boolean;
+  cardBackground: string;
+
+  descricaoCartao: string;
+  titularCartao: string;
+  numeroCartao: string;
+  bancoCartao: string;
+  bandeiraCartao: string;
+  tipoCartao: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.isCadastro = true;
+    this.descricaoCartao = '';
+    this.titularCartao = '';
+    this.numeroCartao = '';
+    this.bancoCartao = '';
+    this.bandeiraCartao = '';
+    this.tipoCartao = 'Credito';
+  }
+
+  alterarCorCartao(): string {
+    this.cardBackground =
+    this.bancoCartao === 'Itaú' ? 'bg-itau' :
+      this.bancoCartao === 'Banco do Brasil' ? 'bg-banco-brasil' :
+      this.bancoCartao === 'Bradesco' || this.bancoCartao === 'Santander' ? 'bg-bradesco' :
+      this.bancoCartao === 'Nubank' ? 'bg-nubank' :
+      this.bancoCartao === 'Caixa Econômica' ? 'bg-caixa-economica' : 'bg-secondary';
+
+    return this.cardBackground;
   }
 
 }
